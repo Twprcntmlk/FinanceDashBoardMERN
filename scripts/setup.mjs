28a589c910d2a8a342d12a108ffb3612cf330e20
Stephen Choung <stephen.choung.coder@gmail.com>
2023-09-24 21:15:00 -0400

    removed fake data load

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -11,41 +11,6 @@ const setup = async () => {
     client = new MongoClient(process.env.MONGODB_URI);
     await client.connect();
 
-    const hasData = await client
-      .db('test')
-      .collection('users')
-      .countDocuments();
-
-    if (hasData) {
-      console.log('Database already exists with data');
-      client.close();
-      return;
-    }
-
-    const records = [...Array(10)].map(() => {
-      const [fName, lName] = faker.name.findName().split(' ');
-      const username = faker.internet.userName(fName, lName);
-      const email = faker.internet.email(fName, lName);
-      const image = faker.image.people(640, 480, true);
-
-      return {
-        name: `${fName} ${lName}`,
-        username,
-        email,
-        image,
-        followers: 0,
-        emailVerified: null
-      };
-    });
-
-    const insert = await client
-      .db('test')
-      .collection('users')
-      .insertMany(records);
-
-    if (insert.acknowledged) {
-      console.log('Successfully inserted records');
-    }
   } catch (error) {
     return 'Database is not ready yet';
   } finally {
